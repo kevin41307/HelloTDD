7dc00dc35b9ac32cf403d7afe528eb491f4bf40c
kevin41307 <kevin41307@hotmail.com>
2023-09-09 17:14:49 +0800

    add player

---
+++ Assets/Game/Scripts/Players/Main/PlayerCharacter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Scripts.Players.Main
+{
+    public class PlayerCharacter : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Game/Scripts/Players/Main/PlayerCharacterInstaller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Game.Scripts.Players.Main
+{
+    public class PlayerCharacterInstaller : MonoInstaller
+    {
+
+        public override void InstallBindings()
+        {
+
+        }
+    }
+}
